b92f6b05195ad3a33b907bbe7d92d0da0d9cfff4
vintech05 <kelanjutannyakelvin@gmail.com>
2023-12-09 17:37:39 +0700

    removed unused code

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 const shareBtnOn = document.querySelector('.card-cta__share-on');
-const shareBtnOff = document.querySelector('.card-cta__share-off');
 const cardFooter = document.querySelector('.card-footer');
-const cardFooterContent = document.querySelector('.card-footer__avatar');
 const cardIcon = document.querySelector('.card-cta');
 
 function stateActive () {
-    cardFooter.style.backgroundColor = '#47556b';
+    cardIcon.style.backgroundColor = '#47556b';
+    cardFooter.style.marginTop = '0.45em';
     cardIcon.style.display = 'flex';
-    cardFooterContent.style.display = 'none';
-}
-
-function stateInactive () {
-    cardFooter.style.backgroundColor = '#fff';
-    cardIcon.style.display = 'none';
-    cardFooterContent.style.display = 'flex';
 }
 
 shareBtnOn.addEventListener('click', stateActive);
-shareBtnOff.addEventListener('click', stateInactive);
